5f1e237d299ef118840eea2ab27501e0502e2170
juanma rivero <45921725+riverojm@users.noreply.github.com>
2022-04-07 15:07:28 -0300

    termine el ejercicio 2

--- Assets/SCRIPTS/CalculoDinero.cs
+++ Assets/SCRIPTS/CalculoDinero.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class CalculoDinero : MonoBehaviour
 {
+    // 1. Crear las variables necesarias para ingresar por inspector el precio de 3 productos y un monto de dinero disponible
+    //    Comunicar si la suma de los mismos supera o no el monto de dinero disponible. Luego indicar cuanto dinero sobra o falta.
     // Start is called before the first frame update
+    public float PrecioProd1;
+    public float PrecioProd2;
+    public float PrecioProd3;
+    float Dinerototal;
+    float Resto;
+    public float DineroDisponible;
     void Start()
     {
-        
+        Dinerototal = PrecioProd3 + PrecioProd2 + PrecioProd1;
+        if (Dinerototal > DineroDisponible)
+        {
+            Resto = Dinerototal - DineroDisponible;
+            Debug.Log("Te sobran $"+Resto+"");
+        }
+        if (Dinerototal < DineroDisponible)
+        {
+            Resto = DineroDisponible - Dinerototal;
+            Debug.Log("Te faltan $"+Resto+"");
+        }
+        {
+
+        }
     }
 
     // Update is called once per frame
--- Assets/SCRIPTS/VariablesIguales.cs
+++ Assets/SCRIPTS/VariablesIguales.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class VariablesIguales : MonoBehaviour
 {
     // 1. Crear un programa que permita ingresar por inspector dos valores enteros y responda si son iguales o no lo son.
-    // 1. Crear las variables necesarias para ingresar por inspector el precio de 3 productos y un monto de dinero disponible
-    //    Comunicar si la suma de los mismos supera o no el monto de dinero disponible. Luego indicar cuanto dinero sobra o falta.
     // Start is called before the first frame update
     public int numero1;
     public int numero2;
